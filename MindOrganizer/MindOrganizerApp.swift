61dc3729d3849d9b07af42a02bcf7bbb6c3c0d97
Reza Daneshvar <rdaneshvar95@gmail.com>
2021-02-09 18:10:51 +0100

    xcode project created with ios and swiftui config

---
+++ MindOrganizer/MindOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindOrganizerApp.swift
+//  MindOrganizer
+//
+//  Created by Reza Daneshvar on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MindOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
